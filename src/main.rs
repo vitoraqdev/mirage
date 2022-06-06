d8ec2cbfc59df464566a99ad0b4fd3654e69978b
vitoraqdev <80975912+vitoraqdev@users.noreply.github.com>
2022-06-05 21:33:24 -0300

    cryptography abstraction

--- src/main.rs
+++ src/main.rs
@@ -39,21 +39,57 @@ enum Methods {
 }
 
 struct Vigenere;
+struct Vernam;
+struct Caesar;
+trait Cryptography<T> {
+    fn encrypt(text: &str, key: T) -> String;
+    fn decrypt(text: &str, key: Option<T>) -> String;
+}
 
-impl Vigenere {
+impl Cryptography<&str> for Vigenere {
+    /// Encrypt a text using the Vigenere cipher using the given key
     fn encrypt(text: &str, key: &str) -> String {
         let mut cipher = String::new();
         
         return cipher;
     }
 
-    fn decrypt(text: &str, key: &str) -> String {
+    /// Decrypt a text using the Vigenere cipher using the given key
+    /// If not given the key, it will brute force it
+    fn decrypt(text: &str, key: Option<&str>) -> String {
         let mut cipher = String::new();
         
         return cipher;
     }
 }
 
+impl Cryptography<&str> for Vernam {
+    /// Encrypt a text using the Vernam cipher using the given key
+    fn encrypt(text: &str, key: &str) -> String {
+        let mut cipher = String::new();
+        
+        return cipher;
+    }
+
+    fn decrypt(text: &str, key: Option<&str>) -> String {
+        todo!()
+    }
+    
+}
+
+impl Cryptography<u8> for Caesar {
+    /// Encrypt a text using the Caesar cipher using the given key
+    fn encrypt(text: &str, key: u8) -> String {
+        let mut cipher = String::new();
+        
+        return cipher;
+    }
+
+    fn decrypt(text: &str, key: Option<u8>) -> String {
+        todo!()
+    }
+    
+}
 
 
 fn main() {
@@ -78,7 +114,7 @@ fn main() {
             println!("Decrypting...");
             match command {
                 Methods::Vigenere { key } => {
-                    println!("{}", Vigenere::decrypt("text", &key));
+                    println!("{}", Vigenere::decrypt("text", Some(&key)));
                 }
                 Methods::Vernam { key } => {
                     todo!()
