99baddacf2feadb5a57d5f7a75331c5801c60dcf
vitoraqdev <80975912+vitoraqdev@users.noreply.github.com>
2022-06-07 16:41:10 -0300

    add non character to test

--- tests/test_caesar.rs
+++ tests/test_caesar.rs
@@ -30,6 +30,10 @@ fn test_encrypt_caesar_with_shift_negative_twenty_seven() {
 fn test_encrypt_caesar_with_non_character() {
     assert_eq!(Caesar::encrypt("Hello!", 1), "Ifmmp!");
 }
+#[test]
+fn test_encrypt_caesar_with_non_character_2() {
+    assert_eq!(Caesar::encrypt("Hello!料", 1), "Ifmmp!料");
+}
 
 
 // Test the decrypt caesar function
@@ -60,4 +64,8 @@ fn test_decrypt_caesar_with_shift_negative_twenty_seven() {
 #[test]
 fn test_decrypt_caesar_with_non_character() {
     assert_eq!(Caesar::decrypt("Ifmmp!", 1), "Hello!");
+}
+#[test]
+fn test_decrypt_caesar_with_non_character_2() {
+    assert_eq!(Caesar::decrypt("Ifmmp!料", 1), "Hello!料");
 }
